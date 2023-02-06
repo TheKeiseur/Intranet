d9c7d62522f97a4f1f2dcb4405a0093f42cb333d
TheKeiseur <arthur.sicard@gmail.com>
2023-02-06 14:56:09 +0100

    [INT-F01](Feat): Initialize components needed for project

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { UserCardComponent } from './shared/user-card/user-card.component';
+import { UserFormComponent } from './shared/user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserCardComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
