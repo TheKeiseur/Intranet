218686849fa8dc5a530765c8e1f6da3f451e3459
TheKeiseur <arthur.sicard@gmail.com>
2023-02-09 15:31:09 +0100

    [INT-F05](Feat): Add edit/update form and apply to pages

--- front/src/app/shared/user-form/user-form.component.ts
+++ front/src/app/shared/user-form/user-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from "../../services/User";
+import {UserService} from "../../services/user.service";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +11,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+  form!: FormGroup;
+  @Input()
+  user?: User;
+  @Input()
+  isAdd = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authService: AuthService,
+              private userService: UserService) {
   }
 
+  ngOnInit(): void {
+    console.log(this.user);
+    this.form = this.formBuilder.group({
+      gender: new FormControl(this.user?.gender, [Validators.required]),
+      category: new FormControl(this.user?.category, [Validators.required]),
+      lastname: new FormControl(this.user?.lastname, [Validators.required]),
+      firstname: new FormControl(this.user?.firstname, [Validators.required]),
+      email: new FormControl(this.user?.email, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required]),
+      confirmPassword: new FormControl(null),
+      phone: new FormControl(this.user?.phone, [Validators.required]),
+      birthdate: new FormControl(this.user?.birthdate, [Validators.required]),
+      city: new FormControl(this.user?.city, [Validators.required]),
+      country: new FormControl(this.user?.country, [Validators.required]),
+      photo: new FormControl(this.user?.photo, [Validators.required]),
+      isAdmin: new FormControl(this.user?.isAdmin),
+    });
+  }
+
+  get email() {
+    return this.form.get('email')!;
+  }
+
+  getErrorMessage() {
+    if (this.email!.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email!.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  submit() {
+    this.isAdd ? this.createUser() : this.updateUser();
+  }
+
+  getIsAdmin(): boolean {
+    return this.authService.getIsAdmin();
+  }
+
+  private createUser() {
+    console.log("Create");
+    const user: User = this.form.value;
+    this.userService.createUser(user).subscribe(() => this.router.navigateByUrl('/users'));
+  }
+
+  private updateUser() {
+    console.log("Update");
+    const user: User = this.form.value;
+    user.id = this.user!.id;
+    this.userService.updateUser(user).subscribe(() => this.router.navigateByUrl('/users'));
+  }
 }
